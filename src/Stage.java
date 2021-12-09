36563c85205d61c0979e32aaa6e7f2709bb918f4
Monkriz <sunseaseen@gmail.com>
2021-12-09 20:39:26 +0300

    Initial commit

---
+++ src/Stage.java
@@ -0,0 +1,13 @@
+public class Stage {
+
+    protected int length;
+    protected String description;
+    public String getDescription() {
+        return description;
+    }
+
+    public void go(Car c) {
+
+    }
+
+}
